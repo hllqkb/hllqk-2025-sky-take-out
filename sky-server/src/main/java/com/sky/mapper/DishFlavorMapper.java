fc6bb34737e96192a2d3eeda010e7b6db2dc6144
hllqkb <androidhtml@yandex.com>
2025-01-04 23:17:36 +0800

    新增修改菜品，待完成redis缓存

--- sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
@@ -2,10 +2,29 @@ package com.sky.mapper;
 
 import com.sky.entity.DishFlavor;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
 @Mapper
 public interface DishFlavorMapper {
+    /**
+     * 批量插入菜品口味
+     * @param list
+     */
     void insertBatch(List<DishFlavor> list);
+    /**
+     * 根据菜品id删除菜品口味
+     * @param id
+     */
+
+    void deleteByDishId(Long id);
+
+    /**
+     * 根据菜品id获取菜品口味
+     * @param id
+     * @return
+     */
+    @Select("SELECT * FROM dish_flavor WHERE dish_id = #{id}")
+    List<DishFlavor> getByDishId(Long id);
 }
